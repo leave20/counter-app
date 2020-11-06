5f4cbd1564e205d684abab6dfc0a7b50d41f9b2b
leave20 <thetai6102@gmail.com>
2020-11-05 23:54:19 -0500

    first class tutorial react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import  React from 'react';
+import PrimeraApp from "./PimeraApp";
+import './index.css' ;
+const divRoot=document.querySelector('#app');
+/**
+ * import react dom
+ * @type {*}
+ */
+const ReactDOM = require('react-dom');
+ReactDOM.render(<PrimeraApp/>,divRoot);
+
